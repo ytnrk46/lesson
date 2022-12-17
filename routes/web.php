1ccec7ea6a1c0509887a813e977b236bef6d4a40
ytnrk46 <ytnrk4060@gmail.com>
2022-12-17 18:14:41 +0900

    投稿詳細画面実装

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [PostController::class, 'index']);
 
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
